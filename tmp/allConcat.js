44420a9b9c6eb59a6df10e294144388d3147fce0
Adam Ross Russell and Marika Allely <mballely@gmail.com>
2016-03-15 15:54:52 -0700

    add separate buttons for celsius and fahrenheit display options

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -20,11 +20,19 @@ $(document).ready(function() {
     $('#location').val("");
     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
       $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
-        $('.showWeather').append("<button class='btn-success' id='temperature'>Temp!</button> ");
+        $('.showWeather').append("<button class='btn-success' id='c_temperature'>Celsius!</button>" + " <button class='btn-success' id='f_temperature'>Fahrenheit!</button>");
 
-        $("#temperature").click(function() {
+        $("#c_temperature").click(function() {
           $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
-            $('.temperature').text("\nThe temperature in celsius is " + cels_converter(response.main.temp) + "\nThe temperature in fahrenheit is " + far_converter(response.main.temp) + ".");
+            $('.c_temperature').text("The temperature in celsius is " + cels_converter(response.main.temp) + ".");
+          }).fail(function(error) {
+            $('.showWeather').text(error.responseJSON.message);
+          });
+        });
+
+        $("#f_temperature").click(function() {
+          $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
+            $('.f_temperature').text("The temperature in fahrenheit is " + far_converter(response.main.temp) + ".");
           }).fail(function(error) {
             $('.showWeather').text(error.responseJSON.message);
           });
